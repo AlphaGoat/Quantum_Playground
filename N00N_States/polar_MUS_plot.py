71c411b747af0a8b335288b87a134943c9de7186
Peter Thomas <pjtpeterjohnthomas@gmail.com>
2019-04-14 12:34:41 -1000

    Added draft file for Minimum Uncertainty States Plot

---
+++ N00N_States/polar_MUS_plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def draw_coherent_MUS_plot():
+
+    fig = plt.figure()
+    ax = fig.add_subplot(2,1,1)
+    # Plot coherent state MUS
+    ax.quiver(0, 0, 16, 16)
+    ax.Circle((16,16), 
+
+    # Plot squeezed state MUS
+    ax.quiver(0, 0, 
+
+    origin = (0,0)
+    x_r = 16
+    x_y=16
+
+    
